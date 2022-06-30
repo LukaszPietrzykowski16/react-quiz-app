60b1d4e54d530e3077bd3d99772d6857f9ed1829
Lukasz Pietrzykowski <lukaszpietrzykowsk@gmail.com>
2022-06-30 12:21:45 +0200

    added end screen

--- src/App.js
+++ src/App.js
@@ -6,16 +6,36 @@ import questions from './questions.js'
 
 function App() {
 
+  const [game, setGame] = useState(true); 
   const [result, setResult] = useState(0);
+  const [end, setEnd] = useState(1);
+
+  function nextQuestion(e){
+    if (questions[result].answer1[e] === true) {
+      setEnd(end + 1)
+    }
+    if (result === 9) {
+      setGame(false);
+    }
+    setResult(result + 1);
+    // console.log(questions.length) it's 11
+    // console.log(result);
+    console.log(end)
+  }
 
   return (
     <div className="App">
+      <div className={game === true ? 'game-on' : 'game-off'}>
       {questions[result].question}
       <br></br>
-      <button onClick={() => setResult(result + 1)}> {questions[result].answer1} </button>
-      <button onClick={() => setResult(result + 1)}> {questions[result].answer2} </button>
-      <button onClick={() => setResult(result + 1)}> {questions[result].answer3} </button>
-      <button onClick={() => setResult(result + 1)}> {questions[result].answer4} </button>
+      <button onClick={(e) => nextQuestion(1)}> {questions[result].answer1} </button>
+      <button onClick={(e) => nextQuestion(2)}> {questions[result].answer2} </button>
+      <button onClick={(e) => nextQuestion(3)}> {questions[result].answer3} </button>
+      <button onClick={(e) => nextQuestion(4)}> {questions[result].answer4} </button>
+      </div>
+      <div className={game === false ? 'result-on' : 'result-off'}>
+        Your score: {end}
+      </div>
     </div>
   );
 }
